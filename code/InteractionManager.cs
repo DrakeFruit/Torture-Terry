85810a2f04dc35afaad1b91d23b606c5c3b1b294
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2025-02-21 17:10:51 -0800

    Refactor code, this feels like shit atm

--- code/InteractionManager.cs
+++ code/InteractionManager.cs
@@ -5,44 +5,81 @@ using Sandbox.Physics;
 public sealed class InteractionManager : Component
 {
     [Property] Vector3 Gravity { get; set; }
-    [Property] float Damping = 1f;
-
-    GameObject HeldObject;
-    PhysicsBody Body;
-    PhysicsBody CursorBody;
-    Sandbox.Physics.SpringJoint Spring;
-    Vector3 GrabOffset;
-
+    private PhysicsBody HeldBody;
+    private GameObject HeldObject;
+    private PhysicsBody CursorBody;
+    private Sandbox.Physics.FixedJoint GrabJoint;
+    
 	protected override void OnEnabled()
 	{
 		Scene.PhysicsWorld.Gravity = Gravity;
-		CursorBody = new( Scene.PhysicsWorld );
+		
+		Clear();
+		
+		CursorBody = new PhysicsBody( Scene.PhysicsWorld ) { BodyType = PhysicsBodyType.Keyframed };
 	}
+	
 	protected override void OnFixedUpdate()
 	{
-        //trace a ray at the mouse position
-        var tr = Scene.Trace.Ray( Scene.Camera.ScreenPixelToRay( Mouse.Position ), 1000 ).Run();
-        CursorBody.Position = tr.EndPosition.WithX(0);
+		var tr = Scene.Trace.Ray( Scene.Camera.ScreenPixelToRay( Mouse.Position ), 1000 ).Run();
 
-        //Pickup
-        if(Input.Down("attack1") && tr.Hit && tr.GameObject.Tags.Has("pickup") && HeldObject == null && Body == null){
-            HeldObject = tr.GameObject;
-            Spring = PhysicsJoint.CreateSpring( CursorBody, tr.Body, 0, 1 );
-            Spring.SpringLinear = Spring.SpringLinear with { Damping = Damping };
-        }
+		if ( Input.Down( "attack1" ) ) Pickup( tr );
+		else if ( HeldBody.IsValid() ) Drop( tr );
 
-        //Drop / Trashing
-        if( !Input.Down( "attack1" ) ){
-	        if( Spring.IsValid() ) Spring.Remove();
-	        if(tr.GameObject.Tags.Has("trash") && Input.Released("attack1"))
-	        {
-		        Spring.Body2.GetGameObject().Destroy();
-	        }
+		CursorBody.Position = tr.EndPosition.WithX( 0 );
+	}
 
-	        HeldObject = null;
-	        Body = null;
-        }
+	public void Pickup(SceneTraceResult tr)
+	{
+		if ( !tr.Hit || tr.Body is null || tr.Body.BodyType == PhysicsBodyType.Static ) return;
 
-        //TODO: play some oof sounds
-    }
+		HeldBody = tr.Body;
+		HeldObject = tr.GameObject;
+
+		var localOffset = HeldBody.Transform.PointToLocal( tr.HitPosition );
+
+		GrabJoint?.Remove();
+		GrabJoint = PhysicsJoint.CreateFixed( new PhysicsPoint( CursorBody ), new PhysicsPoint( HeldBody ) );
+		GrabJoint.Point1 = new PhysicsPoint( CursorBody );
+		GrabJoint.Point2 = new PhysicsPoint( HeldBody, localOffset );
+
+		var maxForce = 100.0f * tr.Body.Mass * Scene.PhysicsWorld.Gravity.Length;
+		GrabJoint.SpringLinear = new PhysicsSpring( 25, 5, maxForce );
+		GrabJoint.SpringAngular = new PhysicsSpring( 0, 0, 0 );
+	}
+
+	public void Drop(SceneTraceResult tr)
+	{
+		GrabJoint?.Remove();
+		GrabJoint = null;
+		HeldBody = null;
+		HeldObject = null;
+			
+		if(tr.GameObject.Tags.Has("trash") && Input.Released("attack1"))
+		{
+			GrabJoint?.Body2.GetGameObject().Destroy();
+		}
+	}
+	
+	private void Clear()
+	{
+		GrabJoint?.Remove();
+		GrabJoint = null;
+
+		CursorBody?.Remove();
+		CursorBody = null;
+
+		HeldBody = null;
+		HeldObject = null;
+	}
+	
+	protected override void OnDisabled()
+	{
+		Clear();
+	}
+	
+	protected override void OnDestroy()
+	{
+		Clear();
+	}
 }
