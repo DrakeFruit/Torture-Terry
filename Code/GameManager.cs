958db8683381903c140705c2c8d75b8b0a111d30
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2025-02-25 20:01:49 -0800

    play-time based achievements

--- Code/GameManager.cs
+++ Code/GameManager.cs
@@ -1,10 +1,12 @@
 ﻿using Sandbox;
+using Sandbox.Services;
 
 namespace TortureTerry;
 public class GameManager : Component
 {
 	public static PlayerData Player = new();
 	private RealTimeSince LastSave = 0f;
+	private RealTimeSince TimePlayed = 0f;
 	protected override void OnStart()
 	{
 		Player = PlayerData.Load();
@@ -12,10 +14,16 @@ public class GameManager : Component
 	
 	protected override void OnUpdate()
 	{
-		if ( LastSave > 1 )
+		if ( LastSave.Relative > 1 )
 		{
 			Player.Save();
 			LastSave = 0;
+		}
+
+		if ( TimePlayed.Relative >= 60 )
+		{
+			Stats.Increment( "minutes_played", 1 );
+			TimePlayed = 0;
 		}
 	}
 }
