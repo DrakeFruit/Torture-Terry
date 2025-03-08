99f404fb7e4593c6a4d19fe2b941561267e8794f
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2025-03-07 20:55:57 -0800

    ShortStack sounds, m4a1, m700, all weapons are automatic

--- Code/Items/Gun.cs
+++ Code/Items/Gun.cs
@@ -6,11 +6,12 @@ public class Gun : Component
 	[Property] public float Damage { get; set; } = 5;
 	[Property] public float PushForce { get; set; } = 1;
 	[Property] public float Cooldown { get; set; } = 0.1f;
-	[Property] public bool Automatic { get; set; } = false;
+	[Property] public int Capacity { get; set; } = 8;
 	
 	[Property] public GameObject Barrel { get; set; }
 	[Property] public GameObject MuzzleFlash { get; set; }
 	[Property] public SoundEvent FireSound { get; set; }
+	[Property] public SoundEvent ReloadSound { get; set; }
 
 	[Property, ToggleGroup( "ManualChambering" )] public bool ManualChambering { get; set; } = false;
 	[Property, Group( "ManualChambering" )] public SoundEvent ChamberSound { get; set; }
@@ -18,23 +19,41 @@ public class Gun : Component
 	public DamageInfo DamageInf { get; set; } = new();
 	public TimeSince Time { get; set; }
 	public SceneTraceResult Tr { get; set; }
+	public SoundHandle ReloadHandle { get; set; }
+	public bool Reloading { get; set; }
+	public int Ammo { get; set; }
 
 	protected override void OnStart()
 	{
+		Ammo = Capacity;
 		DamageInf.Damage = Damage;
 		DamageInf.Attacker = GameObject;
 		DamageInf.Weapon = GameObject;
 	}
 	protected override void OnFixedUpdate()
 	{
-		if ( Input.Pressed( "attack2" ) && Time >= Cooldown || Input.Down( "attack2" ) && Automatic && Time >= Cooldown )
+		if ( ReloadHandle.IsValid() && ReloadHandle.Finished )
 		{
-			Shoot();
+			Ammo = Capacity;
+			Reloading = false;
 		}
+		
+		if ( Input.Down( "attack2" ) && Time >= Cooldown) Shoot();
 	}
 
 	void Shoot()
 	{
+		if ( Reloading ) return;
+		if ( Ammo <= 0 )
+		{
+			Reloading = true;
+			ReloadSound.UI = true;
+			ReloadHandle = Sound.Play( ReloadSound );
+			return;
+		}
+		
+		Ammo--;
+		
 		var tr = Scene.Trace
 			.Ray( Barrel.WorldPosition, Barrel.WorldPosition + Barrel.WorldRotation.Left * 1000f )
 			.IgnoreGameObjectHierarchy( GameObject )
@@ -45,7 +64,7 @@ public class Gun : Component
 		flash.LocalScale = 4;
 		GameManager.Destroy( flash, 2 );
 		FireSound.UI = true;
-		Sound.Play( FireSound );
+		var snd = Sound.Play( FireSound );
 
 		if ( ManualChambering )
 		{
