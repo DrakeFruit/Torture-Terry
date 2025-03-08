fdc6c08a3e8cf4e848e5865f4fd44869cac9eed2
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2025-03-07 16:32:42 -0800

    Bigger muzzle flash

--- Code/Items/Gun.cs
+++ Code/Items/Gun.cs
@@ -6,6 +6,7 @@ public class Gun : Component
 	[Property] public float Damage { get; set; } = 5;
 	[Property] public float PushForce { get; set; } = 10;
 	[Property] public float Cooldown { get; set; } = 0.1f;
+	[Property] public bool Automatic { get; set; } = false;
 	[Property] public GameObject Barrel { get; set; }
 	[Property] public GameObject MuzzleFlash { get; set; }
 	[Property] public SoundEvent FireSound { get; set; }
@@ -21,29 +22,37 @@ public class Gun : Component
 	}
 	protected override void OnFixedUpdate()
 	{
-		if ( Input.Pressed( "attack2" )  && time >= Cooldown )
+		if ( Input.Pressed( "attack2" ) && time >= Cooldown || Input.Down( "attack2" ) && Automatic && time >= Cooldown )
 		{
-			var tr = Scene.Trace
-				.Ray( Barrel.WorldPosition, Barrel.WorldPosition + Barrel.WorldRotation.Left * 1000f )
-				.IgnoreGameObjectHierarchy( GameObject )
-				.Run();
-
-			var flash = MuzzleFlash.Clone( Barrel.WorldPosition );
-			GameManager.Destroy( flash, 2 );
-			FireSound.UI = true;
-			Sound.Play( FireSound );
-
-			if ( tr.Body.IsValid() )
-			{
-				DamageInf.Position = tr.Body.Position;
-				tr.Body.ApplyImpulse( tr.Direction.Normal * 50000 );
-				var terry = tr.GameObject.GetComponent<Terry>();
-				
-				if ( terry.IsValid() ) terry.OnDamage( DamageInf );
-			}
-
-			time = 0;
+			Shoot();
 		}
+	}
+
+	private void Shoot()
+	{
+		var tr = Scene.Trace
+			.Ray( Barrel.WorldPosition, Barrel.WorldPosition + Barrel.WorldRotation.Left * 1000f )
+			.IgnoreGameObjectHierarchy( GameObject )
+			.Run();
+		
+		//Effects
+		var flash = MuzzleFlash.Clone( Barrel.WorldPosition + Barrel.WorldRotation.Left * 8f );
+		flash.LocalScale = 4;
+		GameManager.Destroy( flash, 2 );
+		FireSound.UI = true;
+		Sound.Play( FireSound );
+		
+		//Damage
+		if ( tr.Body.IsValid() )
+		{
+			DamageInf.Position = tr.Body.Position;
+			tr.Body.ApplyImpulse( tr.Direction.Normal * 50000 );
+			var terry = tr.GameObject.GetComponent<Terry>();
+			
+			if ( terry.IsValid() ) terry.OnDamage( DamageInf );
+		}
+		
+		time = 0;
 	}
 
 	protected override void OnPreRender()
